5c0bfb54c6a770bddc494c8019aa01664504787a
naeemr <naeemr@xgrid.co>
2019-04-17 11:44:00 +0500

    bare minimum structure

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"flag"
+	"time"
+
+	"github.com/golang/glog"
+	kubeinformers "k8s.io/client-go/informers"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/sample-controller/pkg/signals"
+)
+
+var (
+	masterURL  string
+	kubeconfig string
+)
+
+func main() {
+	flag.Parse()
+
+	// set up signals so we handle the first shutdown signal gracefully
+	stopCh := signals.SetupSignalHandler()
+
+	cfg, err := clientcmd.BuildConfigFromFlags(masterURL, kubeconfig)
+	if err != nil {
+		glog.Fatalf("Error building kubeconfig: %s", err.Error())
+	}
+
+	kubeClient, err := kubernetes.NewForConfig(cfg)
+	if err != nil {
+		glog.Fatalf("Error building kubernetes clientset: %s", err.Error())
+	}
+	kubeInformerFactory := kubeinformers.NewSharedInformerFactory(kubeClient, time.Second*30)
+	// pass to controller
+	go kubeInformerFactory.Start(stopCh)
+	// if err = controller.Run(2, stopCh); err != nil {
+	// 	glog.Fatalf("Error running controller: %s", err.Error())
+	// }
+	<-stopCh
+}
+
+func init() {
+	flag.StringVar(&kubeconfig, "kubeconfig", "", "Path to a kubeconfig. Only required if out-of-cluster.")
+	flag.StringVar(&masterURL, "master", "", "The address of the Kubernetes API server. Overrides any value in kubeconfig. Only required if out-of-cluster.")
+}
---
+++ pkg/controller/controller.go
@@ -0,0 +1,72 @@
+package controller
+
+import (
+	"github.com/golang/glog"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	appsinformers "k8s.io/client-go/informers/apps/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/kubernetes/scheme"
+	typedcorev1 "k8s.io/client-go/kubernetes/typed/core/v1"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/tools/record"
+	"k8s.io/client-go/util/workqueue"
+)
+
+const controllerAgentName = "ControllerTemplate"
+
+const (
+	SuccessSynced         = "Synced"
+	ErrResourceExists     = "ErrResourceExists"
+	MessageResourceExists = "Resource %q already exists and is not managed by <controllerAgentName> controller"
+	MessageResourceSynced = "Resource<name> synced successfully"
+)
+
+type Controller struct {
+	name          string
+	namespace     string
+	kubeclientset kubernetes.Interface
+	// statefulsetsLister appslisters.StatefulSetLister
+	// statefulsetsSynced cache.InformerSynced
+	workqueue workqueue.RateLimitingInterface
+	recorder  record.EventRecorder
+}
+
+func NewController(
+	kubeclientset kubernetes.Interface,
+	deploymentInformer appsinformers.DeploymentInformer,
+) *Controller {
+	eventBroadcaster := record.NewBroadcaster()
+	eventBroadcaster.StartLogging(glog.Infof)
+	eventBroadcaster.StartRecordingToSink(&typedcorev1.EventSinkImpl{Interface: kubeclientset.CoreV1().Events("")})
+	recorder := eventBroadcaster.NewRecorder(scheme.Scheme, corev1.EventSource{Component: controllerAgentName})
+	controller := &Controller{
+		name:          controllerAgentName,
+		namespace:     "default",
+		kubeclientset: kubeclientset,
+		workqueue:     workqueue.NewRateLimitingQueue(workqueue.DefaultControllerRateLimiter()),
+		recorder:      recorder,
+	}
+	glog.Info("Setting up event handlers")
+	deploymentInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: func(obj interface{}) {
+			// Object is newly created.
+			// enqueue object for processing
+			// controller.enqueue(obj)
+		},
+		UpdateFunc: func(old, new interface{}) {
+			newDep := new.(*appsv1.Deployment)
+			oldDep := new.(*appsv1.Deployment)
+			if newDep.ResourceVersion == oldDep.ResourceVersion {
+				return
+			}
+			// enqueue object for processing
+			// controller.enqueue(new)
+		},
+		DeleteFunc: func(obj interface{}) {
+			// object is being deleted perform custom logic on it
+			// controller.enqueue(obj)
+		},
+	})
+	return controller
+}
