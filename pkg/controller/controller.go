43c48cb98de66b13d17444d7ca2e8b8d145a7e2f
naeemr <naeemr@xgrid.co>
2019-04-17 11:55:25 +0500

    add example of deployment controlller

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -8,6 +8,7 @@ import (
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/kubernetes/scheme"
 	typedcorev1 "k8s.io/client-go/kubernetes/typed/core/v1"
+	appslisters "k8s.io/client-go/listers/apps/v1"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/record"
 	"k8s.io/client-go/util/workqueue"
@@ -22,30 +23,36 @@ const (
 	MessageResourceSynced = "Resource<name> synced successfully"
 )
 
+// This example is about deployment, but it could be any kubernetes resource e.g statefulset, ingress, secrets, configmaps etc.
+// Controller is establish a basic bare bone structure to implement more complex logic to control and respond to kubernetes
+// object lifecycle events.
 type Controller struct {
-	name          string
-	namespace     string
-	kubeclientset kubernetes.Interface
-	// statefulsetsLister appslisters.StatefulSetLister
-	// statefulsetsSynced cache.InformerSynced
-	workqueue workqueue.RateLimitingInterface
-	recorder  record.EventRecorder
+	name             string
+	namespace        string
+	kubeclientset    kubernetes.Interface
+	deploymentLister appslisters.DeploymentLister
+	deploymentSynced cache.InformerSynced
+	workqueue        workqueue.RateLimitingInterface
+	recorder         record.EventRecorder
 }
 
 func NewController(
 	kubeclientset kubernetes.Interface,
 	deploymentInformer appsinformers.DeploymentInformer,
+
 ) *Controller {
 	eventBroadcaster := record.NewBroadcaster()
 	eventBroadcaster.StartLogging(glog.Infof)
 	eventBroadcaster.StartRecordingToSink(&typedcorev1.EventSinkImpl{Interface: kubeclientset.CoreV1().Events("")})
 	recorder := eventBroadcaster.NewRecorder(scheme.Scheme, corev1.EventSource{Component: controllerAgentName})
 	controller := &Controller{
-		name:          controllerAgentName,
-		namespace:     "default",
-		kubeclientset: kubeclientset,
-		workqueue:     workqueue.NewRateLimitingQueue(workqueue.DefaultControllerRateLimiter()),
-		recorder:      recorder,
+		name:             controllerAgentName,
+		namespace:        "default",
+		kubeclientset:    kubeclientset,
+		deploymentLister: deploymentInformer.Lister(),
+		deploymentSynced: deploymentInformer.Informer().HasSynced,
+		workqueue:        workqueue.NewRateLimitingQueue(workqueue.DefaultControllerRateLimiter()),
+		recorder:         recorder,
 	}
 	glog.Info("Setting up event handlers")
 	deploymentInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
